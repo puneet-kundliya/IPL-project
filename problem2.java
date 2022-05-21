b01fad508c86239b456fdf422fb62b75c0bc9c37
Puneet Kundliya <89841826+puneet-kundliya@users.noreply.github.com>
2022-05-21 12:58:28 +0530

    Add files via upload

---
+++ problem2.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class problem2 {
+
+    public static void main(String[] args) {
+
+        HashMap<String , Integer> map = new HashMap<>();
+
+        String csvFile = "/home/puneet/Downloads/matches.csv";
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+        int skip = 0;
+
+        try {
+            br = new BufferedReader(new FileReader(csvFile));
+
+            while((line = br.readLine()) != null) {
+                if (skip == 0) {
+                    ++skip;
+                } else {
+                    String[] match = line.split(cvsSplitBy);
+
+                    if(map.containsKey(match[10])){
+                        map.put(match[10], map.get(match[10])+1);
+                    }
+                    else{
+                        map.put(match[10], 1);
+                    }
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(map);
+    }
+}
